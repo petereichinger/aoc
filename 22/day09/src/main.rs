330afd649bdd65663fa38c0787a97a69d6668215
Peter Eichinger <peter.eichinger@gmail.com>
2022-12-09 16:55:40 +0100

    day09

---
+++ 22/day09/src/main.rs
@@ -0,0 +1,56 @@
+use std::{collections::HashSet, io};
+
+use utils_22::Coord;
+
+fn coord_from_char(dir: char) -> Coord {
+    match dir {
+        'R' => Coord { x: 1, y: 0 },
+        'U' => Coord { x: 0, y: 1 },
+        'L' => Coord { x: -1, y: 0 },
+        'D' => Coord { x: 0, y: -1 },
+        _ => panic!("Invalid dir {}", dir),
+    }
+}
+
+fn main() {
+    rope_move(2);
+    rope_move(10);
+}
+
+fn rope_move(num_knots: usize) {
+    const INPUT: &str = include_str!("input");
+
+    let mut knots = vec![Coord::default(); num_knots];
+
+    let mut visited_s = HashSet::new();
+    visited_s.insert(Coord::default());
+
+    for instruction in INPUT.lines() {
+        let (dir, count) = instruction.split_once(' ').unwrap();
+
+        let dir = coord_from_char(dir.chars().next().unwrap());
+        let count = count.parse::<i32>().unwrap();
+
+        for _ in 0..count {
+            knots[0] += dir;
+            for idx in 1..knots.len() {
+                let k = &mut knots[idx - 1..=idx];
+                let (curr_h, curr_s) = k.split_at_mut(1);
+
+                let curr_h = &mut curr_h[0];
+                let curr_s = &mut curr_s[0];
+
+                if curr_h.distance_to(&curr_s) < 2 {
+                    continue;
+                }
+
+                let diff = *curr_h - *curr_s;
+
+                *curr_s += diff.limit_to_neighbour();
+            }
+            visited_s.insert(knots[knots.len() - 1]);
+        }
+    }
+
+    println!("{} {}", num_knots, visited_s.len());
+}
--- 22/utils/src/lib.rs
+++ 22/utils/src/lib.rs
@@ -1,14 +1,84 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    fmt::Display,
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
+
+#[derive(Default, Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub struct Coord {
+    pub x: i32,
+    pub y: i32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub const ZERO: Coord = Coord { x: 0, y: 0 };
+pub const UP: Coord = Coord { x: 0, y: 1 };
+pub const DOWN: Coord = Coord { x: 0, y: -1 };
+pub const LEFT: Coord = Coord { x: -1, y: 0 };
+pub const RIGHT: Coord = Coord { x: 1, y: 0 };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn diff(a: i32, b: i32) -> u32 {
+    (a - b).unsigned_abs()
+}
+
+fn normalize(a: i32) -> i32 {
+    if a == 0 {
+        a
+    } else {
+        a / a.abs()
+    }
+}
+
+impl Coord {
+    pub fn distance_to(&self, other: &Self) -> u32 {
+        let x_diff = diff(self.x, other.x);
+        let y_diff = diff(self.y, other.y);
+
+        u32::max(x_diff, y_diff)
+    }
+
+    pub fn limit_to_neighbour(&self) -> Self {
+        let x = normalize(self.x);
+        let y = normalize(self.y);
+
+        Coord { x, y }
+    }
+}
+
+impl Add<Coord> for Coord {
+    type Output = Coord;
+
+    fn add(self, rhs: Coord) -> Self::Output {
+        Coord {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl AddAssign<Coord> for Coord {
+    fn add_assign(&mut self, rhs: Coord) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub<Coord> for Coord {
+    type Output = Coord;
+
+    fn sub(self, rhs: Coord) -> Self::Output {
+        Coord {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl SubAssign<Coord> for Coord {
+    fn sub_assign(&mut self, rhs: Coord) {
+        *self = *self - rhs;
+    }
+}
+
+impl Display for Coord {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
     }
 }
