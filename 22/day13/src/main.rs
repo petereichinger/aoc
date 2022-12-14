e2f2f52592e14811d1c7a8e21c6d4767eb06be0a
Peter Eichinger <peter.eichinger@gmail.com>
2022-12-14 07:43:58 +0100

    day13

--- 22/day13/src/main.rs
+++ 22/day13/src/main.rs
@@ -1,5 +1,49 @@
 mod packet;
 
+use std::cmp::Ordering;
+
+use packet::Packet;
+
 fn main() {
-    println!("Hello, world!");
+    const INPUT: &str = include_str!("input");
+
+    let sum_part1 = INPUT
+        .split("\n\n")
+        .enumerate()
+        .filter_map(|(idx, pair)| {
+            let (f, s) = pair.split_once('\n').unwrap();
+
+            let (f, s) = (Packet::from(f), Packet::from(s));
+
+            if f.cmp(&s) == Ordering::Less {
+                Some(idx + 1)
+            } else {
+                None
+            }
+        })
+        .sum::<usize>();
+
+    println!("part 1: {}", sum_part1);
+
+    let mut packets: Vec<Packet> = INPUT
+        .lines()
+        .filter_map(|line| {
+            if line.is_empty() {
+                return None;
+            }
+            return Some(Packet::from(line));
+        })
+        .collect();
+
+    let first = Packet::from("[[2]]");
+    let second = Packet::from("[[6]]");
+    packets.push(first.clone());
+    packets.push(second.clone());
+
+    packets.sort();
+
+    let first_index = packets.iter().position(|packet| packet == &first).unwrap() + 1;
+    let second_index = packets.iter().position(|packet| packet == &second).unwrap() + 1;
+
+    println!("second part {}", first_index * second_index);
 }
--- 22/day13/src/packet.rs
+++ 22/day13/src/packet.rs
@@ -1,44 +1,96 @@
-#[derive(Debug, PartialEq, Eq)]
+const NUMBER_DELIMITER: &[char] = &[',', ']'];
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Packet {
     Integer(i32),
     List(Vec<Packet>),
 }
 
-const NUMBER_DELIMITER: &[char] = &[',', ']'];
+fn parse(mut input: &str) -> (&str, Packet) {
+    let mut items = Vec::new();
+
+    while !input.is_empty() {
+        let mut chars = input.chars();
+        if let Some((next_char, rest)) = chars.next().map(|c| (c, chars.as_str())) {
+            match next_char {
+                '[' => {
+                    let (rest, item) = parse(rest);
+                    items.push(item);
+                    input = rest;
+                }
+                ']' => {
+                    return (&rest, Packet::List(items));
+                }
+                ',' => input = rest,
+                x if x.is_numeric() => {
+                    let number_end = input.find(NUMBER_DELIMITER).unwrap_or(input.len());
+                    let number = input[..number_end].parse::<i32>().unwrap();
+                    items.push(Packet::Integer(number));
+                    input = &input[number_end..];
+                }
+                _ => panic!(),
+            }
+        }
+    }
+
+    assert_eq!(1, items.len());
+
+    return ("", items.pop().unwrap());
+}
 
 impl<T> From<T> for Packet
 where
     T: AsRef<str> + std::fmt::Debug,
 {
-    fn from(s: T) -> Self {
-        #[cfg(test)]
-        eprintln!("{s:?}");
+    fn from(input: T) -> Self {
+        let (rest, packet) = parse(input.as_ref());
+        assert!(rest.is_empty());
 
-        let s = s.as_ref();
-        if s.is_empty() {
-            return Packet::List(vec![]);
+        packet
+    }
+}
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn compare_list_to_int(l: &Vec<Packet>, x: &i32) -> std::cmp::Ordering {
+    let x_vec = vec![Packet::Integer(*x)];
+
+    compare_lists(l, &x_vec)
+}
+
+fn compare_lists(l1: &Vec<Packet>, l2: &Vec<Packet>) -> std::cmp::Ordering {
+    use std::cmp::Ordering;
+    let end_result = if l1.len() < l2.len() {
+        Ordering::Less
+    } else if l2.len() < l1.len() {
+        Ordering::Greater
+    } else {
+        Ordering::Equal
+    };
+    for (a, b) in l1.iter().zip(l2.iter()) {
+        let cmp = a.cmp(b);
+        if cmp != Ordering::Equal {
+            return cmp;
         }
-        let first_char = s.chars().nth(0).unwrap();
+    }
 
-        match first_char {
-            '[' => {
-                let list_end = s.rfind(']').unwrap();
+    end_result
+}
 
-                let inner_list = &s[1..list_end];
-                if inner_list.is_empty() {
-                    Packet::List(vec![])
-                } else {
-                    let items: Vec<Packet> =
-                        s[1..list_end].split(',').map(|item| item.into()).collect();
-
-                    Packet::List(items)
-                }
-            }
-            '0'..='9' => {
-                let number = s.parse::<i32>().unwrap();
-                Packet::Integer(number)
-            }
-            _ => panic!(),
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self {
+            Packet::Integer(x) => match other {
+                Packet::Integer(y) => x.cmp(y),
+                Packet::List(l) => compare_list_to_int(l, x).reverse(),
+            },
+            Packet::List(l) => match other {
+                Packet::Integer(x) => compare_list_to_int(l, x),
+                Packet::List(l2) => compare_lists(l, l2),
+            },
         }
     }
 }
@@ -47,15 +99,11 @@ where
 mod test {
 
     use super::Packet;
+    use std::cmp::Ordering;
 
     #[test]
     fn empty_list_works() {
         assert_eq!(Packet::List(vec![]), "[]".into())
-    }
-
-    #[test]
-    fn simple_integer_works() {
-        assert_eq!(Packet::Integer(3), "3".into())
     }
 
     #[test]
@@ -98,5 +146,98 @@ mod test {
             ]),
             "[[1,2],[1,1]]".into()
         )
+    }
+
+    #[test]
+    fn even_more_complex() {
+        assert_eq!(
+            Packet::List(vec![
+                Packet::List(vec![
+                    Packet::Integer(1),
+                    Packet::Integer(2),
+                    Packet::List(vec![Packet::Integer(4)])
+                ]),
+                Packet::List(vec![Packet::Integer(1), Packet::Integer(1)])
+            ]),
+            "[[1,2,[4]],[1,1]]".into()
+        )
+    }
+
+    #[test]
+    fn verify_test_case_packets_work() {
+        const INPUT: &str = include_str!("test");
+
+        for line in INPUT.lines() {
+            if !line.is_empty() {
+                let _packet = Packet::from(line);
+            }
+        }
+    }
+    #[test]
+    fn verify_input_packets_work() {
+        const INPUT: &str = include_str!("input");
+
+        for line in INPUT.lines() {
+            if !line.is_empty() {
+                let _packet = Packet::from(line);
+            }
+        }
+    }
+
+    #[test]
+    fn compare_simple_ordering() {
+        assert_eq!(Ordering::Equal, Packet::from("[]").cmp(&Packet::from("[]")))
+    }
+
+    #[test]
+    fn order_0_1_elements() {
+        assert_eq!(Ordering::Less, Packet::from("[]").cmp(&Packet::from("[1]")))
+    }
+
+    #[test]
+    fn order_1_0_elements() {
+        assert_eq!(
+            Ordering::Greater,
+            Packet::from("[1]").cmp(&Packet::from("[]"))
+        )
+    }
+
+    #[test]
+    fn order_number_elements() {
+        assert_eq!(
+            Ordering::Less,
+            Packet::from("[1]").cmp(&Packet::from("[2]"))
+        )
+    }
+
+    #[test]
+    fn order_equal_number_elements() {
+        assert_eq!(
+            Ordering::Equal,
+            Packet::from("[1]").cmp(&Packet::from("[1]"))
+        )
+    }
+
+    #[test]
+    fn test_cases() {
+        let test: (Packet, Packet) = ("[1,1,3,1,1]".into(), "[1,1,5,1,1]".into());
+        assert_eq!(Ordering::Less, test.0.cmp(&test.1));
+        let test: (Packet, Packet) = ("[[1],[2,3,4]]".into(), "[[1],4]".into());
+        assert_eq!(Ordering::Less, test.0.cmp(&test.1));
+        let test: (Packet, Packet) = ("[9]".into(), "[[8,7,6]]".into());
+        assert_eq!(Ordering::Greater, test.0.cmp(&test.1));
+        let test: (Packet, Packet) = ("[[4,4],4,4]".into(), "[[4,4],4,4,4]".into());
+        assert_eq!(Ordering::Less, test.0.cmp(&test.1));
+        let test: (Packet, Packet) = ("[7,7,7,7]".into(), "[7,7,7]".into());
+        assert_eq!(Ordering::Greater, test.0.cmp(&test.1));
+        let test: (Packet, Packet) = ("[]".into(), "[3]".into());
+        assert_eq!(Ordering::Less, test.0.cmp(&test.1));
+        let test: (Packet, Packet) = ("[[[]]]".into(), "[[]]".into());
+        assert_eq!(Ordering::Greater, test.0.cmp(&test.1));
+        let test: (Packet, Packet) = (
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]".into(),
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]".into(),
+        );
+        assert_eq!(Ordering::Greater, test.0.cmp(&test.1));
     }
 }
